64a9c9bb7d6699466d635d103aaa4049251ee44e
maicondutradev <109883278+maicondutradev@users.noreply.github.com>
2025-09-26 16:36:46 -0300

    CRUD completo

---
+++ SistemaAcademico/SistemaAcademico.Dominio/Estudante.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaAcademico.Dominio
+{
+    public class Estudante
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Nota { get; set; }
+
+        public Estudante(string nome, decimal nota)
+        {
+            Nome = nome;
+            Nota = nota;
+        }
+
+        public bool EstaAprovado() => Nota >= 7;
+    }
+}
---
+++ SistemaAcademico/SistemaAcademico.Servico/EstudanteServico.cs
@@ -0,0 +1,35 @@
+﻿using SistemaAcademico.Dominio;
+using SistemaAcademico.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaAcademico.Servico
+{
+    public class EstudanteServico : IEstudanteServico
+    {
+        private readonly IEstudanteRepositorio _repositorio;
+
+        // 💡 Injeção de dependência via construtor
+        public EstudanteServico(IEstudanteRepositorio repositorio)
+        {
+            _repositorio = repositorio;
+        }
+
+        public void AdicionarEstudante(string nome, decimal nota)
+        {
+            var estudante = new Estudante(nome, nota);
+            _repositorio.Salvar(estudante);
+        }
+
+        public List<Estudante> ObterTodos() => _repositorio.ObterTodos();
+
+        public Estudante ObterPorId(int id) => _repositorio.ObterPorId(id);
+
+        public void AtualizarEstudante(Estudante estudante) => _repositorio.Atualizar(estudante);
+
+        public void RemoverEstudante(int id) => _repositorio.Remover(id);
+    }
+}
